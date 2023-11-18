028941165754510bd5c6a9f2f5dc701735dfe8a5
open-book29 <anujmaurya704@gmail.com>
2023-11-18 11:57:08 +0530

    change in header

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { About } from "@/components/About";
-import { Contact } from "@/components/Contact";
 import { Experience } from "@/components/Experience";
+import { Contact } from "@/components/Contact";
 import { Intro } from "@/components/Intro";
 import { Projects } from "@/components/Projects";
 import { SectionDivider } from "@/components/SectionDivider";
